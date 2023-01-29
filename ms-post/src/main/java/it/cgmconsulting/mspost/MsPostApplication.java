7f7bcb685f35e0660fbf82e8bbf8c9370ecb5c64
CiccioFr <cicciofr.net@tiscali.it>
2023-01-29 15:57:33 +0100

    Creaz. Spring Initializr MicroServizio ms-post

---
+++ ms-post/src/main/java/it/cgmconsulting/mspost/MsPostApplication.java
@@ -0,0 +1,13 @@
+package it.cgmconsulting.mspost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPostApplication.class, args);
+	}
+
+}
