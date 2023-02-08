3e8b2eff9300b7212d636f4b3c717cc5522fe880
CiccioFr <cicciofr.net@tiscali.it>
2023-02-08 23:09:06 +0100

    Creaz. MicroService Backup

---
+++ ms-backup/src/main/java/it/cgmconsulting/msbackup/MsBackupApplication.java
@@ -0,0 +1,13 @@
+package it.cgmconsulting.msbackup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBackupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBackupApplication.class, args);
+	}
+
+}
